3f2fcefdf860bf5c331e3a7bdc2eb39804cde6f9
HOP-404 <162901615+HOP-404@users.noreply.github.com>
2024-07-04 09:18:19 +0600

    Update HOP-GREEN.py

--- HOP-GREEN.py
+++ HOP-GREEN.py
@@ -1,1 +1,8 @@
-import swag
+import os,platform
+os.system('git pull')
+os.system("clear")
+fbd=platform.architecture()[0]
+if fbd=="32bit":
+    __import__("swag32")
+elif fbd=="64bit":
+    __import__("swag")
